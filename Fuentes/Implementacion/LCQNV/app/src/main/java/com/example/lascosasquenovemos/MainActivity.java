d2880cee82f67c182cf8de4f674344dbc5acd0a0
JohanCH430 <82397552+JohanCH430@users.noreply.github.com>
2022-03-03 21:37:24 +0100

    Creación de carpetas e implementación de Firebase

--- Fuentes/Implementacion/LCQNV/app/src/main/java/com/example/lascosasquenovemos/MainActivity.java
+++ Fuentes/Implementacion/LCQNV/app/src/main/java/com/example/lascosasquenovemos/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.lascosasquenovemos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference mDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase mDatabaseInstance = FirebaseDatabase.getInstance(getString(R.string.firebase_realtime_database_URL));
+        mDatabaseInstance.setPersistenceEnabled(true);
+        mDatabase = mDatabaseInstance.getReference();
+
+
+//Write
+        /*
+        mDatabase.child("Estudiantes").setValue("Johan").addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("firebase", e.getLocalizedMessage());
+            }
+        });
+        */
+
+//Always Listener
+        /*
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                Log.d("firebase", "Value is: " + map);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w("firebase", "loadPost:onCancelled", databaseError.toException());
+            }
+        };
+        mDatabase.addValueEventListener(postListener);
+         */
+
+//Get value
+        /*
+        mDatabase.child("Estudiantes").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                }
+            }
+        });
+         */
     }
+
+
 }
