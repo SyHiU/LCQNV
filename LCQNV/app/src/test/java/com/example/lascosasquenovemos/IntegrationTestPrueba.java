58ed20515c2d49b9de59462cd677bf10a8cdcd86
JohanCH430 <82397552+JohanCH430@users.noreply.github.com>
2022-03-13 19:56:27 +0100

    Pruebas

--- LCQNV/app/src/test/java/com/example/lascosasquenovemos/IntegrationTestPrueba.java
+++ LCQNV/app/src/test/java/com/example/lascosasquenovemos/IntegrationTestPrueba.java
@@ -1,34 +1,28 @@
 package com.example.lascosasquenovemos;
 
-import android.content.Context;
-
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
-
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
-
-import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.when;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
 
 
-@RunWith(JUnit4.class)
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(JUnit4.class)
 @PrepareForTest({ FirebaseDatabase.class})
 public class IntegrationTestPrueba {
 
@@ -38,14 +32,22 @@ public class IntegrationTestPrueba {
 
     @Before
     public void setupTests(){
+        try{
+            mockedDatabaseReference = Mockito.mock(DatabaseReference.class);
 
-        mockedDatabaseReference = Mockito.mock(DatabaseReference.class);
+            FirebaseDatabase mockedFirebaseDatabase = Mockito.mock(FirebaseDatabase.class);
+            when(mockedFirebaseDatabase.getReference()).thenReturn(mockedDatabaseReference);
 
-        FirebaseDatabase mockedFirebaseDatabase = Mockito.mock(FirebaseDatabase.class);
-        when(mockedFirebaseDatabase.getReference()).thenReturn(mockedDatabaseReference);
+            PowerMockito.mockStatic(FirebaseDatabase.class);
+            when(FirebaseDatabase.getInstance()).thenReturn(mockedFirebaseDatabase);
+        } catch(Exception e){
+            if(e.getCause().getClass().equals(AssertionError.class)){
+                // handle your exception  1
+            } else {
+                // handle the rest of the world exception
+            }
+        }
 
-        PowerMockito.mockStatic(FirebaseDatabase.class);
-        when(FirebaseDatabase.getInstance()).thenReturn(mockedFirebaseDatabase);
 
     }
 
